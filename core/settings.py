4138dc268e96ff1b6ad30c97364df12f33e95aec
Matheus Borges Balieiro <matheusb.25@hotmail.com>
2023-06-11 20:17:58 -0300

    +

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l@_wtbac%vi%*fvo-a86glsp=vr9wm&+q1l_mrt8e$&!#$!g62"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 
@@ -113,11 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_files"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 
 # Default primary key field type
