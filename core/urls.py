8a977186e1aa3fa30ebd80e03c8d07e9fbf8a356
Matheus Borges Balieiro <matheusb.25@hotmail.com>
2023-06-11 19:23:31 -0300

    +

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
+from app.home import home_views
 
 urlpatterns = [
-    path("", include("home.urls")),
     path("admin/", admin.site.urls),
+    path("home/", home_views),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
--- home/views.py
+++ home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, "index.html")
+def home_view(request):
+    return render(request, "home.html", {})
 
 
 def about(request):
@@ -12,19 +12,3 @@ def about(request):
 
 def contact(request):
     return render(request, "pages/contact.html")
-
-
-def error404(request):
-    return render(request, "pages/404.html")
-
-
-def profile(request):
-    return render(request, "accounts/profile.html")
-
-
-def login(request):
-    return render(request, "accounts/login.html")
-
-
-def register(request):
-    return render(request, "accounts/register.html")
