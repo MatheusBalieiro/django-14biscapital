86a20a647467e276cf55cb425a284291d1a72196
Matheus Borges Balieiro <matheusb.25@hotmail.com>
2023-06-11 18:44:08 -0300

    +

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static/"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/var/app/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
